4624b469a9ad1ac7cb675bfbadb3117e2eee52d6
Viktor <vvgrygorchuk@gmail.com>
2022-11-08 18:37:32 +0200

    Add requirements and globals.

---
+++ globals.py
@@ -0,0 +1,3 @@
+MAX_LEN = 512
+MODEL_ABS_PATH = '/home/mudro/Documents/Projects/en-ukr-translater/models/saved/en_uk_legal_translater'
+CURATED_DATASET_PATH = '/home/mudro/Documents/Projects/en-ukr-translater/dataset/curated'
